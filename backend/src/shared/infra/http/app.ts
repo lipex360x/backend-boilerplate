0134f63720f3d2eedd6cd0487248aeeeea4774d2
Felipe Borges <lipex360@gmail.com>
2020-12-01 21:48:08 -0300

    StorageProvider Added

--- backend/src/shared/infra/http/app.ts
+++ backend/src/shared/infra/http/app.ts
@@ -6,6 +6,8 @@ import express, { NextFunction, Request, Response } from 'express'
 import { errors } from 'celebrate'
 
 import '@shared/containers'
+import storageConfig from '@shared/containers/providers/StorageProvider/config/storage.config'
+
 import routes from '@shared/infra/http/routes'
 import connectDB from '@shared/infra/typeorm'
 
@@ -16,6 +18,9 @@ const app = express()
 
 app.use(express.json())
 app.use(routes)
+
+app.use('/files', express.static(storageConfig.uploadFolder))
+
 app.use(errors())
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
