7e91d8e2de7f5164f9f4014a3987bc4a030d6a0a
Felipe Borges <lipex360@gmail.com>
2020-12-27 02:34:56 -0300

    TypeORM Connect updated

--- backend/src/shared/infra/typeorm/index.ts
+++ backend/src/shared/infra/typeorm/index.ts
@@ -1,17 +1,24 @@
 import { createConnections } from 'typeorm'
+import promiseRetry from '@shared/utils/PromiseRetry'
 
-const connectDB = async () => {
-  try {
-    const connect = await createConnections()
+class OrmConnect {
+  public async execute () {
+    try {
+      const connect = await createConnections()
+      console.log(`💖 Connected to ${connect[0].options.database}`)
 
-    console.log(`💖 Connected to ${connect[0].options.database}`)
-
-    process.on('SIGINT', () => {
-      connect[0].close().then(() => console.log(`  💔 Disconnected to ${connect[0].options.database}`))
-    })
-  } catch (error) {
-    console.log('Database Error: ', error.message)
+      process.on('SIGINT', () => {
+        connect[0].close().then(() => console.log(`  💔 Disconnected to ${connect[0].options.database}`))
+      })
+    } catch (error) {
+      await promiseRetry.execute({
+        timeInSeconds: 2,
+        attemptLimit: 10,
+        message: 'Trying to connect to database',
+        functionRetry: () => { return this.execute() }
+      })
+    }
   }
 }
 
-export default connectDB
+export default new OrmConnect().execute()
