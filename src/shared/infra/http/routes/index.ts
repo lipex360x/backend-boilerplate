3704508ca2e0c2d492e360bcb76fbd4bd9235e4c
Felipe Borges <lipex360@gmail.com>
2021-01-24 20:15:36 -0300

    Session Midware added

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 
+import authRoutes from '@shared/middlewares/authRoutes/routes/auth.routes'
 // import module.routes from '@module/.../infra/http/routes/module.routes.ts'
 
 const routes = Router()
@@ -9,5 +10,7 @@ const routes = Router()
 routes.get('/', (request, response) => {
   response.json({ message: 'Hello World' })
 })
+
+routes.use('/auth', authRoutes)
 
 export default routes
